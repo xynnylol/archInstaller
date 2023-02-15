4dc85c11f4fa975768fdf6c22403a7b0d29003f6
xynny <86185324+xynnylol@users.noreply.github.com>
2023-02-15 22:24:04 +0000

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -4,7 +4,7 @@
 clear;
 echo -e "* Please type the name (example: sda) of the disk you want to use (THIS WILL ERASE ALL CONTENTS)\n" && 
 lsblk -a;
-echo "* Disk name: " && read disk;
+echo -n "* Disk name: " && read disk;
 echo -n "* Please type 'CONFIRM' to confirm that you want to use $disk for arch linux. (I AM NOT RESPONIBLE FOR DATA LOSS.) THE DISK YOU HAVE CHOSEN WILL BE FULLY WIPED:" && read yn && echo "";
 if [ "$yn" != "CONFIRM" ]; then
 exit;
